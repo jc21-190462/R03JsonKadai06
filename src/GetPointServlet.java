7d5b1a741219e0dfc6d20a198756a1e2298f3b38
190151 <190151@e0505-31.JC-21.local>
2022-02-01 10:09:24 +0900

    getpoint初回で５００を表示するよう変更

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -38,54 +38,61 @@ public class GetPointServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		final String driverName = "com.mysql.jdbc.Driver";
 		final String url = "jdbc:mysql://192.168.54.190:3306/jsonkadai06";
 		final String id = "jsonkadai06";
 		final String pass = "JsonKadai06";
-		
+
 		try {
 			Class.forName(driverName);
-			Connection connection=DriverManager.getConnection(url,id,pass);
-			PreparedStatement st = connection.prepareStatement(
-							"select point from POINT where TENPO_ID=? AND USER_ID=?"
-					//"select * from point"
-						);
-			String sa=request.getParameter("TENPO_ID");
-			String a=request.getParameter("USER_ID");
+			Connection connection = DriverManager.getConnection(url, id, pass);
+			PreparedStatement st = connection.prepareStatement("select point from POINT where TENPO_ID=? AND USER_ID=?"
+			// "select * from point"
+			);
+			String sa = request.getParameter("TENPO_ID");
+			String a =  request.getParameter("USER_ID");
 			st.setString(1, sa);
 			st.setString(2, a);
 			ResultSet result = st.executeQuery();
-			
-			List<String[]> list=new ArrayList<>();
-			
-			if( result.next() == true) {
-				String[] s=new String[1];
-				s[0]=result.getString("point");
-				list.add(s);	
-			}else {
-				PreparedStatement st2 = connection.prepareStatement(
-						"insert into point(TENPO_ID,USER_ID,POINT) values(?,?,500)"
-					);
+
+			List<String[]> list = new ArrayList<>();
+
+			if (result.next() == true) {
+				String[] s = new String[1];
+				s[0] = result.getString("point");
+				list.add(s);
+			} else {
+				PreparedStatement st2 = connection
+						.prepareStatement("insert into point(TENPO_ID,USER_ID,POINT) values(?,?,500)");
 				st2.setString(1, sa);
 				st2.setString(2, a);
-				
-				int x=st2.executeUpdate();
-				if(x == 1) {
+
+				int x = st2.executeUpdate();
+
+				if (x == 1) {
 					System.out.println("新規追加成功");
-				}System.out.println("新規追加失敗");
+					st.setString(1, sa);
+					st.setString(2, a);
+					result = st.executeQuery();
+					if (result.next() == true) {
+						String[] s = new String[1];
+						s[0] = result.getString("point");
+						list.add(s);
+					}
+
+				} else {
+					System.out.println("新規追加失敗");
+				}
 			}
-			
-			for(String n[]:list) {
-				System.out.println(n[0]);
-			}
+
 			request.setAttribute("list", list);
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
 			rd.forward(request, response);
-		} catch (ClassNotFoundException e ) {
+		} catch (ClassNotFoundException e) {
 			// TODO 閾ｪ蜍慕函謌舌＆繧後◆ catch 繝悶Ο繝�繧ｯ
 			e.printStackTrace();
-		} catch (SQLException e ) {
+		} catch (SQLException e) {
 			// TODO 閾ｪ蜍慕函謌舌＆繧後◆ catch 繝悶Ο繝�繧ｯ
 			e.printStackTrace();
 		}
